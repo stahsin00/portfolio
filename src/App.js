b68bf03cc5d9f273a9de127144f52fcb11ea74b5
stahsin00 <lamisa1998@hotmail.com>
2024-03-17 18:58:02 -0700

    added navigation between pages #5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Nav from './components/Nav';
 import Socials from './components/Socials';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Nav />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+            <Route path='/' element={<About />} exact/>
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/resume' element={<Resume />} />
+          </Routes>
+      </BrowserRouter>
       <Socials />
     </>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Nav() {
     return (
         <nav className='fixed p-5 space-x-5'>
-            <button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Overview</button>
-            <button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Projects</button>
-            <button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Resume</button>
+            <Link to='/'><button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Overview</button></Link>
+            <Link to='/projects'><button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Projects</button></Link>
+            <Link to='/resume'><button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Resume</button></Link>
         </nav>
     );
 }
