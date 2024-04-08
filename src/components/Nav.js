53be1e1959b2e0fc4ce1a83bb9f5858331b2178a
stahsin00 <lamisa1998@hotmail.com>
2024-04-07 19:38:35 -0700

    increased background contrast

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 function Nav() {
     return (
         <nav className='fixed p-5 space-x-5'>
-            <Link to='/'><button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Overview</button></Link>
-            <Link to='/projects'><button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Projects</button></Link>
-            <Link to='/resume'><button className='bg-blue-400 hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Resume</button></Link>
+            <Link to='/portfolio/'><button className='bg-black hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Overview</button></Link>
+            <Link to='/portfolio/projects'><button className='bg-black hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Projects</button></Link>
+            <Link to='/portfolio/resume'><button className='bg-black hover:bg-blue-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full'>Resume</button></Link>
         </nav>
     );
 }
