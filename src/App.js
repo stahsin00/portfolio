1cd1d8e92129ca688e1ddba260cf7964ccf9d99f
Shushama Tahsin <124312583+stahsin00@users.noreply.github.com>
2024-04-07 19:02:23 -0700

    plz work ;-;

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter, Route, Routes, } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename='portfolio'>
         <Nav />
         <Routes>
             <Route path='/' element={<About />} exact/>
