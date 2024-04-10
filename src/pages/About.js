d10bccac2cbef8aebfb68b6217ed98a1a4fbf28c
BuildTools <lamisa1998@hotmail.com>
2024-04-09 19:11:25 -0700

    changed projects page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,12 +5,7 @@ function About() {
     return (
         <div className="bg-cover bg-center h-screen w-full flex justify-center items-center" style={{ backgroundImage: `url(${backgroundImage})`, imageRendering: 'pixelated' }}>
             <div className='text-white text-2xl bg-black bg-opacity-80 rounded p-5 max-w-4xl'>
-                <p>
-                    Hello! I'm Shushama, a Computer Science student currently enrolled at Langara. This website 
-                    was created to showscase my projects. My primary interests include game development and 
-                    web development. Going further, I'd also love to learn more about cybersecurity and have started 
-                    my journey through the CompTIA certifications.
-                </p>
+                Coming Soon.
             </div>
         </div>
     );
