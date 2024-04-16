3969a1973309d817baae4d248e4c707edfab4ef0
BuildTools <lamisa1998@hotmail.com>
2024-04-15 17:35:09 -0700

    adjusted sizing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 function About() {
     return (
-        <div className="h-screen w-full p-20 flex justify-center items-center text-3xl font-bold">
+        <div className="h-screen w-full p-20 flex flex-col justify-center items-center text-3xl font-bold">
+            <img src='/portfolio/assets/construction.png' alt='pixel art of a room' className='h-auto w-1/4' style={{ imageRendering: 'pixelated' }} />
             Coming Soon...ish
         </div>
     );
