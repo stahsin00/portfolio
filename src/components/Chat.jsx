3b8b136f7d5c05a0df6a9dbcdcd4d377264b6575
BuildTools <lamisa1998@hotmail.com>
2024-10-07 21:45:19 -0700

    fix chat styling

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -88,14 +88,14 @@ function Chat() {
             </div>
             {
                 isOpen ?
-                <div className='bg-white fixed top-0 left-0 md:top-auto md:left-auto md:bottom-5 md:right-20 space-y-5 md:rounded-lg border border-black shadow-md w-screen h-screen md:w-120 md:h-4/6'>
+                <div className='bg-white fixed top-0 left-0 md:top-auto md:left-auto md:bottom-5 md:right-20 space-y-5 md:rounded-lg border border-black shadow-md w-screen h-screen md:w-120 md:h-4/6 flex flex-col'>
                     <div className='m-0 p-0 pb-2'>
                         <button className="absolute right-0 flex items-center justify-center text-teal-700 p-1 m-1 rounded-full" onClick={() => setIsOPen(!isOpen)}><FaTimes size={18} /></button>
                     </div>
-                    <div ref={chatRef} className='w-full h-[85%] overflow-auto scrollbar-thin scrollbar-thumb-black scrollbar-track-transparent scrollbar-thumb-rounded-lg scrollbar-track-rounded-full'>
+                    <div ref={chatRef} className='w-full flex-grow overflow-auto scrollbar-thin scrollbar-thumb-black scrollbar-track-transparent scrollbar-thumb-rounded-lg scrollbar-track-rounded-full'>
                         {renderChat()}
                     </div>
-                    <div className='absolute bottom-5 flex w-full'>
+                    <div className='pb-5 flex w-full'>
                         <input 
                             className='p-1 rounded-lg border border-gray-300 focus:outline-none w-5/6 ml-5 shadow-inner' 
                             type='text' 
