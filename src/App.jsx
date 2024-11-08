5cc437cc0bee361f97df72e59f4f11c854a9ec9e
stahsin00 <lamisa1998@hotmail.com>
2024-11-07 18:15:09 -0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import Nav from './components/Nav';
 import Socials from './components/Socials';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import { BrowserRouter, Route, Routes, } from "react-router-dom";
+// import { BrowserRouter, Route, Routes, } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
 import './App.css'
 import Contact from './pages/Contact';
 import Home from './pages/Home';
@@ -12,16 +14,16 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Nav />
         <Routes>
-          <Route path='/portfolio/' element={<Projects />} exact/>
-          <Route path='/portfolio/home' element={<Home />} />
-          <Route path='/portfolio/about' element={<About />} />
-          <Route path='/portfolio/projects' element={<Projects />} />
-          <Route path='/portfolio/contact' element={<Contact />} />
+          <Route path='/' element={<Projects />} exact/>
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Socials />
       <Chat />
     </>
