8ae996b563dce9017e781b366f28957822197a36
stahsin00 <lamisa1998@hotmail.com>
2025-01-08 14:23:42 -0800

    responsiveness

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -28,20 +28,20 @@ function Carousel() {
   }
 
   return (
-    <div className='bg-black/75 rounded-lg border border-white w-4/5 h-3/4 2xl:h-4/5 flex flex-col'>
-        <div className='text-white pt-7 2xl:pt-10 pl-20 pr-20 w-full'>
-            <h1 className='text-white text-3xl 2xl:text-5xl font-bold [text-shadow:_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_2px_2px_0_#000] flex items-center'><img src="/portfolio/sunflower.ico" alt="sunflower" className='h-8 2xl:h-11 w-auto mr-2' style={{imageRendering: 'pixelated'}}></img> Welcome to my <span className='text-yellow-300 ml-3'>Portfolio</span></h1>
+    <div className='bg-black/75 rounded-lg border border-white w-11/12 md:w-4/5 h-3/4 2xl:h-4/5 flex flex-col'>
+        <div className='text-white pt-5 md:pt-7 2xl:pt-10 px-5 md:pl-20 md:pr-20 w-full'>
+            <h1 className='text-white text-lg md:text-3xl 2xl:text-5xl font-bold [text-shadow:_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_2px_2px_0_#000] flex items-center'><img src="/portfolio/sunflower.ico" alt="sunflower" className='h-8 2xl:h-11 w-auto mr-2' style={{imageRendering: 'pixelated'}}></img> Welcome to my <span className='text-yellow-300 ml-3'>Portfolio</span></h1>
             <div className='text-lg 2xl:text-xl h-7 2xl:h-10 w-52 2xl:w-64 p-2 mt-2 mb-2 2xl:mb-5 bg-black rounded-lg font-vt323 text-teal-400  border-b border-r border-gray-700 flex items-center'><Typewriter text='Hello! :) '/></div>
             <hr></hr>
         </div>
         <div className='w-full flex flex-col items-center mt-3 2xl:mt-10'>
-            <h2 className='text-white text-xl 2xl:text-2xl font-bold'>Featured Projects</h2>
-            <div className='flex justify-center items-center pt-2 2xl:pt-5 pb-2 2xl:pb-10 w-full h-64 2xl:h-120'>
-                <button className='text-white p-10'><FaAngleLeft size={32} onClick={onLeftClick} /></button>
-                <div className='flex text-white justify-around w-4/5' onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
-                    <img src={`/portfolio/assets/${featuredProjects[curIndex].image}`} alt={featuredProjects[curIndex].name} className="w-2/4 2xl:w-3/5 h-auto transform transition-transform duration-300 hover:scale-105" style={{imageRendering: featuredProjects[curIndex].pixelated ? 'pixelated' : 'auto'}} />
-                    <div className='w-1/4'>
-                        <h3 className="text-xl 2xl:text-3xl font-bold mb-2 2xl:mb-5">{featuredProjects[curIndex].name}</h3>
+            <h2 className='text-white text-xl 2xl:text-2xl font-bold mb-2 md:mb-0'>Featured Projects</h2>
+            <div className='flex justify-center items-center py-2 2xl:pt-5 2xl:pb-10 w-full h-80 md:h-64 2xl:h-120'>
+                <button className='hidden md:block text-white p-10'><FaAngleLeft size={32} onClick={onLeftClick} /></button>
+                <div className='flex flex-col md:flex-row text-white justify-around w-4/5' onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
+                    <img src={`/portfolio/assets/${featuredProjects[curIndex].image}`} alt={featuredProjects[curIndex].name} className="w-full md:w-2/4 2xl:w-3/5 h-auto transform transition-transform duration-300 hover:scale-105" style={{imageRendering: featuredProjects[curIndex].pixelated ? 'pixelated' : 'auto'}} />
+                    <div className='w-full md:w-1/4'>
+                        <h3 className="text-xl 2xl:text-3xl font-bold mt-2 md:mt-0 mb-2 2xl:mb-5">{featuredProjects[curIndex].name}</h3>
                         <hr className='p-1 2xl:p-2'></hr>
                         <div className="text-sm 2xl:text-xl font-bold mb-2">{featuredProjects[curIndex].techStack.join(', ')}</div>
                         <p className='mt-2 2xl:mt-5 mb-2 2xl:mb-5'>{featuredProjects[curIndex].description}</p>
@@ -58,12 +58,12 @@ function Carousel() {
                             </div>
                     </div>
                 </div>
-                <button className='text-white p-10'><FaAngleRight size={32} onClick={onRightClick}/></button>
+                <button className='hidden md:block text-white p-10'><FaAngleRight size={32} onClick={onRightClick}/></button>
             </div>
-            <div className='text-white flex gap-3'>
+            <div className='text-white flex gap-5 md:gap-3'>
                 {
                     featuredProjects.map((project, index) => (
-                        <button className={`${index == curIndex ? 'text-white' : 'text-gray-400'} hover:text-white text-sm 2xl:text-lg`} onClick={() => {setCurIndex(index)}}>
+                        <button className={`${index == curIndex ? 'text-white' : 'text-gray-400'} hover:text-white text-xl md:text-sm 2xl:text-lg`} onClick={() => {setCurIndex(index)}}>
                             <FaCircle/>
                         </button>
                     ))
