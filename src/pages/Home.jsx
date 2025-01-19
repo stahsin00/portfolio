a463be2065a5740a6a973a92c1981f481daa3d35
stahsin00 <lamisa1998@hotmail.com>
2025-01-19 14:15:16 -0800

    parallax

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import Carousel from '../components/Carousel';
 
 function Home() {
     const [day, setDay] = useState(true);
+    const foregroundRef = useRef(null);
+    const midgroundRef = useRef(null);
+    const backgroundRef = useRef(null);
 
     const handleClick = () => {
         setDay(!day);
+    }
+
+    const handleMouseMove = (e) => {
+        if (foregroundRef.current) {
+            const { left, width } = foregroundRef.current.getBoundingClientRect();
+            const x = (e.clientX - left) / width;
+            
+            const moveX = -(x - 0.5) * 30;
+
+            foregroundRef.current.style.transform = `translateX(${moveX}px)`;
+        }
+
+        if (midgroundRef.current) {
+            const { left, width } = midgroundRef.current.getBoundingClientRect();
+            const x = (e.clientX - left) / width;
+            
+            const moveX = -(x - 0.5) * 15;
+
+            midgroundRef.current.style.transform = `translateX(${moveX}px)`;
+        }
+
+        if (backgroundRef.current) {
+            const { left, width } = backgroundRef.current.getBoundingClientRect();
+            const x = (e.clientX - left) / width;
+            
+            const moveX = -(x - 0.5) * 5;
+
+            backgroundRef.current.style.transform = `translateX(${moveX}px)`;
+        }
     }
 
     const bgImage = day ? '/portfolio/assets/home-day.png' : '/portfolio/assets/home-night.png';
@@ -14,7 +46,7 @@ function Home() {
     // <Typewriter text='Hello! :)'/>
 
     return (
-        <>
+        <div onMouseMove={handleMouseMove}>
             <img src='/portfolio/assets/cloud.png' alt='cloud' className='z-20 h-screen fixed bottom-20 hidden lg:block' style={{imageRendering: 'pixelated'}}></img>
             <div className={`h-screen w-full bg-cover bg-[position:18%_center] lg:bg-center bg-no-repeat bg-fixed [image-rendering:pixelated]`} style={{ backgroundImage: `url(${bgImage})` }}>
                 {/* <button className='fixed top-5 right-5 bg-black hover:bg-teal-700 text-white text-lg font-bold py-2 px-4 pl-6 pr-6 rounded-full border-2 border-white border-solid' onClick={handleClick}>TEMP</button> */}
@@ -22,9 +54,13 @@ function Home() {
                     <Carousel/>
                 </div>
             </div>
-            <div className={`fixed top-0 left-0 z-20 h-screen w-full bg-cover bg-[position:67%_center] lg:bg-center bg-no-repeat bg-fixed [image-rendering:pixelated]`} style={{ backgroundImage: `url(/portfolio/assets/home-foreground.png)` }}>
+            <div className={`fixed top-0 right-10 z-0 h-screen w-full bg-cover bg-[position:67%_center] lg:bg-center bg-no-repeat bg-fixed [image-rendering:pixelated] transform transition-transform duration-100 ease-out`} style={{ backgroundImage: `url(/portfolio/assets/home-background.png)` }} ref={backgroundRef}>
             </div>
-        </>
+            <div className={`fixed top-5 left-20 z-0 h-screen w-full bg-cover bg-[position:67%_center] lg:bg-center bg-no-repeat bg-fixed [image-rendering:pixelated] transform transition-transform duration-100 ease-out`} style={{ backgroundImage: `url(/portfolio/assets/home-foreground.png)` }}  ref={midgroundRef}>
+            </div>
+            <div className={`fixed top-0 left-0 z-20 h-screen w-full bg-cover bg-[position:67%_center] lg:bg-center bg-no-repeat bg-fixed [image-rendering:pixelated] transform transition-transform duration-100 ease-out pointer-events-none`} style={{ backgroundImage: `url(/portfolio/assets/home-foreground.png)` }}  ref={foregroundRef}>
+            </div>
+        </div>
     );
 }
 
